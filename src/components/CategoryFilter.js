3e560d6d37256cfe49e17e919c1b921705882581
vaibha987 <v2goyal95@gmail.com>
2025-01-05 22:52:32 +0530

    Update CategoryFilter.js

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,25 +1,16 @@
-// src/App.js
-import React, { useState } from 'react';
-import CategoryFilter from './components/CategoryFilter';
-import DishList from './components/DishList';
-import data from './utils/data';
+// src/components/CategoryFilter.js
+import React from 'react';
 
-function App() {
-  const [selectedCategory, setSelectedCategory] = useState('all');
-
-  const categories = ['all', 'breakfast', 'lunch', 'shakes'];
-
-  const filteredDishes = selectedCategory === 'all'
-    ? data
-    : data.filter(dish => dish.category === selectedCategory);
-
+function CategoryFilter({ categories, setSelectedCategory }) {
   return (
-    <div className="App">
-      <h1>Dish Menu</h1>
-      <CategoryFilter categories={categories} setSelectedCategory={setSelectedCategory} />
-      <DishList data={filteredDishes} />
+    <div className="category-filter">
+      {categories.map(category => (
+        <button key={category} onClick={() => setSelectedCategory(category)}>
+          {category}
+        </button>
+      ))}
     </div>
   );
 }
 
-export default App;
+export default CategoryFilter;
