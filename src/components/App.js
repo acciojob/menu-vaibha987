ecdf620cb191e4b5c26551452e7d27ddc5562a18
vaibha987 <v2goyal95@gmail.com>
2025-01-06 23:21:47 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,129 @@
-// src/App.js
-import React, { useState } from 'react';
-import CategoryFilter from './components/CategoryFilter';
-import DishList from './components/DishList';
-import data from './src/data';
+import React from "react";
+import Card from "./card";
+import { useState } from "react";
+import "../styles/header.css";
 
-function App() {
-  const [selectedCategory, setSelectedCategory] = useState('all');
+let Foodarr =[
+    {
+      id: 1,
+      title: 'buttermilk pancakes',
+      category: 'breakfast',
+      price: 15.99,
+      img: './images/item-1.jpeg',
+      desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    },
+    {
+      id: 2,
+      title: 'diner double',
+      category: 'lunch',
+      price: 13.99,
+      img: './images/item-2.jpeg',
+      desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+    },
+    {
+      id: 3,
+      title: 'godzilla milkshake',
+      category: 'shakes',
+      price: 6.99,
+      img: './images/item-3.jpeg',
+      desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+    },
+    {
+      id: 4,
+      title: 'country delight',
+      category: 'breakfast',
+      price: 20.99,
+      img: './images/item-4.jpeg',
+      desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+    },
+    {
+      id: 5,
+      title: 'egg attack',
+      category: 'lunch',
+      price: 22.99,
+      img: './images/item-5.jpeg',
+      desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+    },
+    {
+      id: 6,
+      title: 'oreo dream',
+      category: 'shakes',
+      price: 18.99,
+      img: './images/item-6.jpeg',
+      desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+    },
+    {
+      id: 7,
+      title: 'bacon overflow',
+      category: 'breakfast',
+      price: 8.99,
+      img: './images/item-7.jpeg',
+      desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+    },
+    {
+      id: 8,
+      title: 'american classic',
+      category: 'lunch',
+      price: 12.99,
+      img: './images/item-8.jpeg',
+      desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+    },
+    {
+      id: 9,
+      title: 'quarantine buddy',
+      category: 'shakes',
+      price: 16.99,
+      img: './images/item-9.jpeg',
+      desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    },
+  ];
 
-  const categories = ['all', 'breakfast', 'lunch', 'shakes'];
 
-  const filteredDishes = selectedCategory === 'all'
-    ? data
-    : data.filter(dish => dish.category === selectedCategory);
+const App =()=>{
+    const [foodItems, setFoodItems]=useState(Foodarr);
 
-  return (
-    <div className="App">
-      <h1>Dish Menu</h1>
-      <CategoryFilter categories={categories} setSelectedCategory={setSelectedCategory} />
-      <DishList data={filteredDishes} />
-    </div>
-  );
+
+    const showAll =()=>{
+      setFoodItems(Foodarr);
+    }
+    
+    
+    const showBreakfast =()=>{
+        setFoodItems(Foodarr.filter((item)=>{
+            return item.category === "breakfast";
+        }));
+    }
+    
+    const showLunch=()=>{
+      setFoodItems(Foodarr.filter((item)=>{
+          return item.category === "lunch";
+      }));
+    }
+    
+    const showShakes =()=>{
+      setFoodItems(Foodarr.filter((item)=>{
+          return item.category == "shakes";
+      }));
+    }
+
+    return(
+        <div>
+            <h1 className="headingTitle">Our Menu</h1>
+            <ul className="headingTabs" type="none">
+                    <li  id="main" className="main" onClick={showAll} >All</li>
+                    <li id="filter-btn-1" className="filter-btn-1" onClick={showBreakfast}>Breakfast</li>
+                    <li id="filter-btn-2" className="filter-btn-2" onClick={showLunch}>Lunch</li>
+                    <li id="filter-btn-3" className="filter-btn-3" onClick={showShakes}>Shakes</li>
+            </ul>
+
+            <div className="Fooddiv">
+              {foodItems.map((item)=>{
+                    return <Card key={item.id} title={item.title} category={item.category} price={item.price} img={item.img} desc={item.desc}  />
+              })}
+            </div>
+        </div>
+    );
+
 }
 
 export default App;
